ad26a77cb04137f034c02983994e06864885db7e
Ichthus Mkt - Agência Win <contatoagenciawin@gmail.com>
2026-09-02 19:24:15 -0300

    Preparar mensuração básica por ambiente

---
+++ src/components/TrackingScripts.tsx
@@ -0,0 +1,32 @@
+import Script from 'next/script'
+
+export function TrackingScripts() {
+  const gaId = process.env.NEXT_PUBLIC_GA_ID
+  const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID
+  const clarityId = process.env.NEXT_PUBLIC_CLARITY_ID
+
+  return (
+    <>
+      {gaId ? (
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
+          <Script id="ga4" strategy="afterInteractive">
+            {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}window.gtag=gtag;gtag('js',new Date());gtag('config','${gaId}');`}
+          </Script>
+        </>
+      ) : null}
+
+      {metaPixelId ? (
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${metaPixelId}');fbq('track','PageView');`}
+        </Script>
+      ) : null}
+
+      {clarityId ? (
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,'clarity','script','${clarityId}');`}
+        </Script>
+      ) : null}
+    </>
+  )
+}
